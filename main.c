2b65572a42672dce672a89113d37948626b259ce
Alexandru Vasilescu <alexandru.vasilescu01@gmail.com>
2021-01-15 13:09:59 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,218 @@
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+// Reading
+
+char *read_line(void)
+{
+    char *line = NULL;
+    size_t bufsize = 0; // have getline allocate a buffer for us
+
+    if (getline(&line, &bufsize, stdin) == -1)
+    {
+        if (feof(stdin))
+        {
+            exit(EXIT_SUCCESS); // We recieved an EOF
+        }
+        else
+        {
+            perror("readline");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    return line;
+}
+
+#define TOK_BUFSIZE 64
+#define TOK_DELIM " \t\r\n\a"
+char **split_line(char *line)
+{
+    int bufsize = TOK_BUFSIZE, position = 0;
+    char **tokens = malloc(bufsize * sizeof(char *));
+    char *token;
+
+    if (!tokens)
+    {
+        fprintf(stderr, "dbsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, TOK_DELIM);
+    while (token != NULL)
+    {
+        tokens[position] = token;
+        position++;
+
+        if (position >= bufsize)
+        {
+            bufsize += TOK_BUFSIZE;
+            tokens = realloc(tokens, bufsize * sizeof(char *));
+            if (!tokens)
+            {
+                fprintf(stderr, "dbsh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        token = strtok(NULL, TOK_DELIM);
+    }
+    tokens[position] = NULL;
+    return tokens;
+}
+
+// Process launcher
+
+int launch(char **args)
+{
+    pid_t pid, wpid;
+    int status;
+
+    pid = fork();
+    if (pid == 0)
+    {
+        // Child process
+        if (execvp(args[0], args) == -1)
+        {
+            perror("dbsh");
+        }
+        exit(EXIT_SUCCESS);
+    }
+    else if (pid < 0)
+    {
+        // Error forking
+        perror("dbsh");
+    }
+    else
+    {
+        // Parent process
+        do
+        {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+
+    return 1;
+}
+
+// Built-ins
+
+/*
+  Function Declarations for builtin shell commands:
+ */
+int cd(char **args);
+int help(char **args);
+int sh_exit(char **args);
+
+/*
+  List of builtin commands, followed by their corresponding functions.
+ */
+char *builtin_str[] = {
+    "cd",
+    "help",
+    "exit"};
+
+int (*builtin_func[])(char **) = {
+    &cd,
+    &help,
+    &sh_exit};
+
+int num_builtins()
+{
+    return sizeof(builtin_str) / sizeof(char *);
+}
+
+/*
+  Builtin function implementations.
+*/
+int cd(char **args)
+{
+    if (args[1] == NULL)
+    {
+        fprintf(stderr, "dbsh: expected argument to \"cd\"\n");
+    }
+    else
+    {
+        if (chdir(args[1]) != 0)
+        {
+            perror("dbsh");
+        }
+    }
+    return 1;
+}
+
+int help(char **args)
+{
+    int i;
+    printf("Wrapper shell for dbxcli\n");
+    printf("The following are built in:\n");
+
+    for (i = 0; i < num_builtins(); i++)
+    {
+        printf("  %s\n", builtin_str[i]);
+    }
+    return 1;
+}
+
+int sh_exit(char **args)
+{
+    return 0;
+}
+
+// Shell executor
+
+int execute(char **args)
+{
+    int i;
+
+    if (args[0] == NULL)
+    {
+        // An empty command was entered.
+        return 1;
+    }
+
+    for (i = 0; i < num_builtins(); i++)
+    {
+        if (strcmp(args[0], builtin_str[i]) == 0)
+        {
+            return (*builtin_func[i])(args);
+        }
+    }
+
+    return launch(args);
+}
+
+// Main shell loop
+
+void loop(void)
+{
+    char *line;
+    char **args;
+    int status;
+
+    do
+    {
+        printf("> ");
+        line = read_line();
+        args = split_line(line);
+        status = execute(args);
+
+        free(line);
+        free(args);
+    } while (status);
+}
+
+int main(int argc, char **argv)
+{
+    // Load config files, if any.
+
+    // Run command loop.
+    loop();
+
+    // Perform any shutdown/cleanup.
+
+    return EXIT_SUCCESS;
+}
